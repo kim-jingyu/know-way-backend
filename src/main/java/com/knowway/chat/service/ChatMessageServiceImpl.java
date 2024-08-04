a8b7594bf88a49c25e8f5346af7ea793483a2e3c
yjpark <ox8764@naver.com>
2024-08-04 15:07:16 +0900

    chore: 불필요한 import문 삭제

--- src/main/java/com/knowway/chat/service/ChatMessageServiceImpl.java
+++ src/main/java/com/knowway/chat/service/ChatMessageServiceImpl.java
@@ -20,7 +20,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
