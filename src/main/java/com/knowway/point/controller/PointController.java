0320cdcc10299e882957a299b8b7a461b6b902a5
JZU0 <ljzh1030@gmail.com>
2024-08-06 12:05:36 +0900

    refactor: points 엔드포인트 수정

--- src/main/java/com/knowway/point/controller/PointController.java
+++ src/main/java/com/knowway/point/controller/PointController.java
@@ -8,11 +8,11 @@ import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/points")
+@RequestMapping("/admin")
 public class PointController {
     private final PointService pointService;
 
-    @PostMapping
+    @PostMapping("/points")
     public ResponseEntity<String> addPoint(@RequestBody PointRequest pointRequest) {
         pointService.addPoint(pointRequest);
         return ResponseEntity.ok().body("포인트 적립이 정상적으로 처리되었습니다.");
